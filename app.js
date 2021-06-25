b84d798dcfd11802d8978f26c169a03b0f14af50
franzji <falbright1@gmail.com>
2021-06-25 13:22:33 -0400

    unzip with callback fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var fileUpload = require('express-fileupload');
 var indexRouter = require('./routes/index');
 var app = express();
+var AdmZip = require('adm-zip');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,39 +24,49 @@ app.use(fileUpload({
 
 //main api call
 app.post('/uploadzip', async (req, res) => {
-    download(req, res);
-    
+  try {
+      if(req.files) {
+        let zipfile = req.files.zipfile;
+        zipfile.mv(zipfile.name, function(err) {   //callback to ensure the download finishes before continuing.
+          if (err) return res.status(500).json(err);
+
+          //unzip the contents
+          unzip(req, res);
+
+          
+          //parse output
+
+          apiReply(res, {
+            message: 'success!',
+          })
+        });
+      } else {
+        apiReply(res, {
+          message: 'failure! No zipfile sent',
+        });
+      }
+  } catch (err) {
+    apiReply(res, {
+      message: 'failure!' + err,
+    });
+  }
 });
 
-function download(req, res){
-    try{
-        if(req.files){
-            let zipfile = req.files.zipfile;
-            zipfile.mv(zipfile.name);
-            res.send({
-                message: 'success!',
-              });
-        }
-        else{
-            res.send({
-                message: 'failure!',
-              });
-        }
-    }
-    catch (err) {
-        res.send({
-            message: 'failure!' + err,
-          });
-      }
+function unzip(){
+	// extract the archives using the unzip module to local folder
+	let zip = new AdmZip("Default Sample File.zip");
+  zip.extractAllTo("./extracted/",true);
 }
 
 
+function apiReply(res, replyResponse){
+  res.send(replyResponse);
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
-  });
-
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
