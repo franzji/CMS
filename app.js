abdded636ecdaf9f82eb7766419845dcf04bd248
franzji <falbright1@gmail.com>
2021-06-25 12:56:40 -0400

    Download zip file to folder

--- app.js
+++ app.js
@@ -3,29 +3,58 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fileUpload = require('express-fileupload');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//file upload
+app.use(fileUpload({
+    createParentPath: true
+  }));
+
+//main api call
+app.post('/uploadzip', async (req, res) => {
+    download(req, res);
+    
+});
+
+function download(req, res){
+    try{
+        if(req.files){
+            let zipfile = req.files.zipfile;
+            zipfile.mv(zipfile.name);
+            res.send({
+                message: 'success!',
+              });
+        }
+        else{
+            res.send({
+                message: 'failure!',
+              });
+        }
+    }
+    catch (err) {
+        res.send({
+            message: 'failure!' + err,
+          });
+      }
+}
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+    next(createError(404));
+  });
 
 // error handler
 app.use(function(err, req, res, next) {
